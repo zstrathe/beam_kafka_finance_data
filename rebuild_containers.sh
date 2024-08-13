46e5dbaa0773455c3411903a6b9b5209a8495ae3
Zack Strathe <zstrathe@outlook.com>
2024-08-13 15:50:13 -0500

    update

--- rebuild_containers.sh
+++ rebuild_containers.sh
@@ -1,4 +1,4 @@
-echo "Starting data pipeline services..."
+#echo "Starting data pipeline services..."
 
 docker compose build --no-cache # rebuild the container images to ensure any changes are pushed
 echo "Finished rebuilding container images!"
@@ -7,6 +7,6 @@ echo "Finished rebuilding container images!"
 # and launch compose watch in a new terminal window assumes that running gnome-terminal (i.e., Ubuntu)
 #docker compose up --force-recreate --remove-orphans && gnome-terminal -x sh -c "docker compose watch"
 
-docker compose up --force-recreate --remove-orphans &
-gnome-terminal -x sh -c "echo Waiting 5 seconds to start compose watch; sleep 5; docker compose watch" &
+#docker compose up --force-recreate --remove-orphans &
+#gnome-terminal -x sh -c "echo Waiting 5 seconds to start compose watch; sleep 5; docker compose watch" &
 
