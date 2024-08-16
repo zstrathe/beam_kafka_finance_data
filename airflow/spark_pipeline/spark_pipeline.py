698ec89bc7e5073c39a0303431755b814fa90d48
Zack Strathe <zstrathe@outlook.com>
2024-08-16 02:21:35 -0500

    update spark pipeline to shut down after idle timeout and add custom offset tracking

--- airflow/spark_pipeline/spark_pipeline.py
+++ airflow/spark_pipeline/spark_pipeline.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import os
+import time
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col, window, mean, to_timestamp
 from pyspark.sql.types import StructType, StructField, StringType, FloatType, TimestampType
@@ -69,7 +70,11 @@ def process_data(spark):
         .trigger(processingTime='5 minutes') \
         .start()
     
+    # monitor query and stop if idle (no new records from kafka) for 2 minutes
+    monitor_and_stop_query_and_log_offsets(query, 60*2)
+
     query.awaitTermination()
+
 
 def write_to_postgres(batch_df, batch_id):
     print(f"Writing batch {batch_id} to Postgres")
@@ -99,6 +104,46 @@ def write_to_postgres(batch_df, batch_id):
     # # Write to PostgreSQL
     batch_df.write \
         .jdbc(url=jdbc_url, table="test_write", mode="append", properties=properties)
+
+def monitor_and_stop_query_and_log_offsets(query, max_idle_time_seconds):
+    last_progress_time = time.time()
+    last_progress_batch_id = 0
+    last_progress_num_input_rows = 0
+
+    while query.isActive:
+        # Wait for some time before checking progress
+        time.sleep(10)  # Check every 10 seconds
+
+        # Get the latest progress
+        latest_progress = query.lastProgress
+        
+        if latest_progress:
+            # look for case when batchId increases, and the number of input rows goes from a positive number to zero
+            # seems reasonably safe to assume that the offset has decreased then and rows have been written to db
+            if latest_progress['batchId'] > last_progress_batch_id and latest_progress['numInputRows'] == 0 and last_progress_num_input_rows > 0:
+                latest_progress_offsets = latest_progress['sources'][0]['endOffset']['data_stream']
+                # make sure the offsets partition and value are stored as ints for comparison
+                latest_progress_offsets = {int(k): int(v) for k, v in latest_progress_offsets.items()}
+                with open('last_offsets', 'w', encoding='utf-8') as f:
+                    f.write(str(latest_progress_offsets))
+                print('Updated offset file!')
+
+            last_progress_batch_id = latest_progress['batchId']
+            last_progress_num_input_rows = latest_progress['numInputRows']
+           
+            # update the last progress time if new data was processed
+            if latest_progress['numInputRows'] > 0:
+                last_progress_time = time.time()
+            
+            # check if idle time threshold is exceeded
+            idle_time = time.time() - last_progress_time
+            if idle_time > max_idle_time_seconds:
+                print(f"No new data received for {idle_time} seconds. Stopping the query.")
+                query.stop()
+                break
+        
+        # Print some progress information
+        print(f"Query is still active. Idle time: {time.time() - last_progress_time} seconds")
 
 def run_pipeline():
     # change current dir to spark_pipeline dev directory
