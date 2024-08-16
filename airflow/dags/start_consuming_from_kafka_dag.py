92d765f6cf91a4d29cb489fa58565f121970de5b
Zack Strathe <zstrathe@outlook.com>
2024-08-15 20:17:29 -0500

    update for running spark pipeline

--- airflow/dags/start_consuming_from_kafka_dag.py
+++ airflow/dags/start_consuming_from_kafka_dag.py
@@ -1,100 +1,170 @@
 import logging
 import socket
+import json
 from datetime import datetime
+import os
 from confluent_kafka import Consumer, TopicPartition
 from airflow import DAG
 from airflow import models
 from airflow.models import TaskInstance
 from airflow.models.dagrun import DagRun
 from airflow.operators.python_operator import PythonOperator, ShortCircuitOperator
-from airflow.providers.apache.beam.operators.beam import BeamRunPythonPipelineOperator
+from airflow.operators.bash import BashOperator
+# from airflow.providers.apache.beam.operators.beam import BeamRunPythonPipelineOperator
 
 from airflow.sensors.python import PythonSensor
 from kafka import BrokerConnection
 from kafka.protocol.commit import *
 
 # add .packages/ directory to sys.path, so that other relative modules can be imported
-import os
-import sys
-
-#sys.path.append(os.path.dirname('/opt/airflow/plugins')) 
+# import os
+# import sys
+# sys.path.append(os.path.dirname('/opt/airflow/spark_pipeline')) 
+# from spark_pipeline.spark_pipeline import run_pipeline
 #from test_operator import BeamRunPythonPipelineOperator
 
 logger = logging.getLogger("airflow.task")
- 
-def check_kafka_topic_new_messages_threshold_sensor(msg_threshold: int):
-    '''airflow sensor to check kafka topic for new messages for specified group_id
+
+class CheckKafkaNewMessagesSensor:
+    ''' airflow sensor to check kafka topic for new messages for specified group_id
         - args: threshold: int: minumum number of new messages required to trigger sensor 
     '''
-    topic_name = 'data_stream'
-    group_id = 'pipeline-consumer'
-    hostname = 'broker'
-    port = 29092
+    def __init__(self, 
+                 topic_name: str = 'data_stream', 
+                 group_id: str = 'pipeline-consumer', 
+                 broker_bootstrap_url: str = 'broker:29092', # broker:29092 inside docker, localhost:9092 outside of docker
+                 new_message_threshold: int = 25,
+                 current_offset_source: str = "",
+                 spark_checkpoints_dir: str = "", 
+                 *args,
+                 **kwargs
+        ) -> None:
 
-    ### FIRST: get the current offset values for each partition in the kafka topic
-    # use python-kafka module for this 
-    # link to source for getting offset data: https://stackoverflow.com/a/49244595
-    bc = BrokerConnection(hostname, port, socket.AF_INET)
-    
-    bc.connect_blocking()
-    
-    fetch_offset_request = OffsetFetchRequest_v3(group_id, None)
-    
-    future = bc.send(fetch_offset_request)
-    
-    while not future.is_done:
-        for resp, f in bc.recv():
-            f.success(resp)
+        self.topic_name = topic_name
+        self.group_id = group_id
+        url = broker_bootstrap_url.split(":")
+        self.url_host = url[0]
+        self.url_port = int(url[1])
+        self.msg_threshold = new_message_threshold
+        
+        if current_offset_source == 'spark' and spark_checkpoints_dir == "":
+            raise ValueError("need to include spark checkpoints dir if using spark offsets!")
+        elif current_offset_source == 'spark':
+            self.current_offset_source = current_offset_source
+            self.spark_checkpoints_dir = spark_checkpoints_dir # "../spark_pipeline/tmp_checkpoints", (RUNNING IN DOCKER VIA AIRFLOW)"spark_pipeline/tmp_checkpoints"
 
-    # setup default for current_offsets if none exist yet (i.e., partition 0, offset 0)
-    current_offsets = {0: 0} 
-    for t in future.value.topics:
-        # t[0] is the topic string
-        if t[0] == topic_name:
-            current_offsets = {
-                # partition data for each topic is organized in a tuple as: 
-                # (partition_number[int], partition_offset[int], ??, ??)
-                d[0]: d[1] for d in t[1]
-            }
-    print("TEST: CURRENT OFFSETS:", current_offsets)
+    @classmethod
+    def _airflow_callable(cls, *args, **kwargs):
+        '''callable method to call with airflow PythonSensor
+        '''
+        instance = cls(*args, **kwargs)
+        return instance.check()
+
+    def check(self) -> bool:
+        ''' get current - high watermark offsets for kafka topic and return true if greater than msg_threshold
+        '''
+        if self.current_offset_source == "kafka":
+            current_offsets = self.get_current_offsets_from_kafka()
+        elif self.current_offset_source == "spark":
+            current_offsets = self.get_current_offsets_from_spark_checkpoint()
+
+        end_offsets = self.get_high_watermark_offsets_from_kafka()
+        
+        # check if there are new messages
+        print("TEST CURRENT AND END OFFSETS:", " curr: ", current_offsets, "; end: ", end_offsets)
+        for partition, current_offset in current_offsets.items():
+            end_offset = end_offsets.get(partition, 0)
+            num_new_messages = end_offset - current_offset
+            if num_new_messages >= self.msg_threshold:
+                # New messages are available
+                print(f'TEST: ABOVE MESSAGE THRESHOLD: {num_new_messages} NEW MESSAGES AVAILABLE IN KAFKA')
+                return True
+            elif num_new_messages > 0:
+                print(f'TEST: BELOW MESSAGE THRESHOLD: {num_new_messages} NEW MESSAGES AVAILABLE IN KAFKA')
+                return False
+        # No new messages found
+        print('NEW MESSAGES ARE NOT AVAILABLE IN KAFKA')
+        return False
+
+    def get_current_offsets_from_spark_checkpoint(self) -> dict:
+        ''' read spark checkpoint file directory to retrieve current offsets 
+            (when running pipeline via spark, because spark tracks offsets separately and does not commit them to kafka)
+        '''
+        max_timestamp = 0
+        max_timestamp_offsets = {0: 0}
+        for (root, _, files) in os.walk(self.spark_checkpoints_dir):  
+            if root.endswith('/offsets'):
+                for file in files:
+                    # read only files that don't have any extension
+                    if not os.path.splitext(file)[1]:
+                        try:
+                            file_path = os.path.join(root, file)
+                            lines = open(file_path).readlines()
+                            ts = json.loads(lines[1]).get('batchTimestampMs')
+                            if ts > max_timestamp:
+                                offsets = json.loads(lines[2]).get(self.topic_name)
+                                # convert the offset key (partition number) to an int
+                                offsets = {int(k): v for k, v in offsets.items()}
+                                max_timestamp = ts
+                                max_timestamp_offsets = offsets
+                        except Exception as e:
+                            print('Error reading file: ', e)
+        print("TEST: spark offsets: ", max_timestamp_offsets)
+        return max_timestamp_offsets
+
+    def get_current_offsets_from_kafka(self) -> dict:
+        ''' 
+        get the current offset values for each partition in the kafka topic
+         - use python-kafka module for this 
+         - link to source for getting offset data: https://stackoverflow.com/a/49244595
+        '''
+        bc = BrokerConnection(self.url_host, self.url_port, socket.AF_INET)
+        
+        bc.connect_blocking()
+        
+        fetch_offset_request = OffsetFetchRequest_v3(self.group_id, None)
+        
+        future = bc.send(fetch_offset_request)
+        
+        while not future.is_done:
+            for resp, f in bc.recv():
+                f.success(resp)
+
+        # setup default for current_offsets if none exist yet (i.e., partition 0, offset 0)
+        current_offsets = {0: 0} 
+        for t in future.value.topics:
+            # t[0] is the topic string
+            if t[0] == self.topic_name:
+                current_offsets = {
+                    # partition data for each topic is organized in a tuple as: 
+                    # (partition_number[int], partition_offset[int], ??, ??)
+                    d[0]: d[1] for d in t[1]
+                }
+
+        return current_offsets
+        
+    def get_high_watermark_offsets_from_kafka(self) -> dict:
+        ''' get the total number of messages for each partition in the kafka topic
+        '''
+        consumer = Consumer({"bootstrap.servers": f"{self.url_host}:{self.url_port}",
+                            "group.id": self.group_id})
     
-    ### SECOND: get the total number of messages for each partition in the kafka topic
-    consumer = Consumer({"bootstrap.servers": f"{hostname}:{port}",
-                         "group.id": group_id})
-  
-    # # Get the partition information for the topic
-    partitions = consumer.list_topics(topic=topic_name).topics[topic_name].partitions
-    topic_partitions = [TopicPartition(topic_name, partition_id) for partition_id in partitions.keys()]
-   
-    # # Assign the partitions to the consumer
-    consumer.assign(topic_partitions)
-
-    # Get the "high watermark"/end offsets for the partitions
-    end_offsets = {
-        # get partition number (key) and partition offset watermarks (low, high), and only select high val
-        partition.partition: consumer.get_watermark_offsets(partition, timeout=5)[1]
-        for partition in topic_partitions
-    }
-    consumer.close()
+        # # Get the partition information for the topic
+        partitions = consumer.list_topics(topic=self.topic_name).topics[self.topic_name].partitions
+        topic_partitions = [TopicPartition(self.topic_name, partition_id) for partition_id in partitions.keys()]
     
-    ### THIRD: Check if there are new messages
-    print("TEST CURRENT AND END OFFSETS:", " curr: ", current_offsets, "; end: ", end_offsets)
-    for partition, current_offset in current_offsets.items():
-        end_offset = end_offsets.get(partition, 0)
-        print('TEST: current_offset:', current_offset, '...end_offset:', end_offset)
-        num_new_messages = end_offset - current_offset
-        if num_new_messages >= msg_threshold:
-            # New messages are available
-            print(f'TEST: ABOVE MESSAGE THRESHOLD: {num_new_messages} NEW MESSAGES AVAILABLE IN KAFKA')
-            return True
-        elif num_new_messages > 0:
-            print(f'TEST: BELOW MESSAGE THRESHOLD: {num_new_messages} NEW MESSAGES AVAILABLE IN KAFKA')
-            return False
+        # # Assign the partitions to the consumer
+        consumer.assign(topic_partitions)
 
-    # No new messages found
-    print('NEW MESSAGES ARE NOT AVAILABLE IN KAFKA')
-    return False
-
+        # Get the "high watermark"/end offsets for the partitions
+        end_offsets = {
+            # get partition number (key) and partition offset watermarks (low, high), and only select high val
+            partition.partition: consumer.get_watermark_offsets(partition, timeout=5)[1]
+            for partition in topic_partitions
+        }
+        consumer.close()
+        return end_offsets
+        
 # default_args = {
 #     'owner': 'your_name',
 #     'depends_on_past': False,
@@ -115,22 +185,27 @@ with DAG(
     catchup=False
     ) as dag:
     
-    # check_kafka_new_messages = PythonSensor(
-    #     task_id='check_if_new_messages_in_kafka_topic',
-    #     python_callable=check_kafka_topic_new_messages_threshold_sensor,
-    #     op_kwargs={'msg_threshold':300},
-    #     mode='reschedule',
-    #     poke_interval=600 # 10 minutes
-    # )
-
-    run_beam_consumer_batch_task = BeamRunPythonPipelineOperator(
-        task_id="start_python_pipeline_local_direct_runner",
-        py_file="/opt/airflow/beam_pipeline/beam_pipeline.py",
-        pipeline_options={"num_messages": 5},
-        runner='DirectRunner',
-        py_interpreter="python3",
-        py_system_site_packages=False,
+    check_kafka_new_messages = PythonSensor(
+        task_id='check_if_new_messages_in_kafka_topic',
+        python_callable=CheckKafkaNewMessagesSensor._airflow_callable,
+        op_kwargs={'current_offset_source': 'spark',
+                   'spark_checkpoints_dir': 'spark_pipeline/tmp_checkpoints'},
+        mode='reschedule',
+        reschedule_interval=60*10
     )
 
-    # check_kafka_new_messages >> 
-    run_beam_consumer_batch_task
+    start_spark_pipeline = BashOperator(
+        task_id="start_python_pipeline_spark",
+        bash_command='python3 ../../opt/airflow/spark_pipeline/spark_pipeline.py',
+    )
+
+    # run_beam_consumer_batch_task = BeamRunPythonPipelineOperator(
+    #     task_id="start_python_pipeline_local_direct_runner",
+    #     py_file="/opt/airflow/beam_pipeline/beam_pipeline.py",
+    #     pipeline_options={"num_messages": 5},
+    #     runner='DirectRunner',
+    #     py_interpreter="python3",
+    #     py_system_site_packages=False,
+    # )
+
+    check_kafka_new_messages >> start_spark_pipeline
--- airflow/spark_pipeline/spark_pipeline.py
+++ airflow/spark_pipeline/spark_pipeline.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import os
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col, window, mean, to_timestamp
 from pyspark.sql.types import StructType, StructField, StringType, FloatType, TimestampType
@@ -10,8 +11,6 @@ def create_spark_session():
         .config("spark.jars", "postgresql-42.6.0.jar") \
         .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2") \
         .config("spark.sql.streaming.checkpointLocation", "./tmp_checkpoints/") \
-        .config("spark.executor.memory", "6g") \
-        .config("spark.driver.memory", "6g") \
         .getOrCreate()
 
 def extract_data(df):
@@ -34,18 +33,19 @@ def process_data(spark):
     df = spark \
         .readStream \
         .format("kafka") \
-        .option("kafka.bootstrap.servers", "localhost:9092") \
+        .option("kafka.bootstrap.servers", "broker:29092") \
         .option("subscribe", "data_stream") \
         .option("group.id", "pipeline-consumer") \
-        .option("auto.offset.reset", "earliest") \
+        .option("startingOffsets", "earliest") \
         .load()
+#  .option("startingOffsets", "earliest") \
 
     # Extract and transform data
     extracted_df = extract_data(df)
 
     # Window and aggregate data
     windowed_df = extracted_df \
-        .withWatermark("timestamp", "2 minutes") \
+        .withWatermark("timestamp", "0 minutes") \
         .groupBy(
             window("timestamp", "5 minutes"),
             "stock_ticker"
@@ -59,48 +59,52 @@ def process_data(spark):
         col("avg_price").alias("price")
     )
 
-    df.writeStream.format("console").start()
+    output_df.writeStream.format("console").start()
 
     # Write to PostgreSQL
     query = output_df \
         .writeStream \
-        .outputMode("complete") \
+        .outputMode("append") \
         .foreachBatch(write_to_postgres) \
         .trigger(processingTime='5 minutes') \
         .start()
-
-    query.awaitTermination(timeout=600)
+    
+    query.awaitTermination()
 
 def write_to_postgres(batch_df, batch_id):
     print(f"Writing batch {batch_id} to Postgres")
     print(f"Number of records: {batch_df.count()}")
     # JDBC properties
-    jdbc_url = "jdbc:postgresql://localhost:5432/test_db"
+    jdbc_url = "jdbc:postgresql://db:5432/test_db"
     properties = {
         "user": "postgres",
         "password": "mysecretpassword",
         "driver": "org.postgresql.Driver"
     }
 
-    # Create a temporary view of the batch data
-    batch_df.createOrReplaceTempView("updates")
+    # # Create a temporary view of the batch data
+    # batch_df.createOrReplaceTempView("updates")
 
-    # Perform the upsert operation
-    upsert_sql = """
-    INSERT INTO test_write (stock_ticker, timestamp, price)
-    VALUES (stock_ticker, timestamp, price)
-    ON CONFLICT(stock_ticker, timestamp)
-    DO UPDATE SET price = EXCLUDED.price
-    """
+    # # Perform the upsert operation
+    # upsert_sql = """
+    # INSERT INTO test_write (stock_ticker, timestamp, price)
+    # VALUES (stock_ticker, timestamp, price)
+    # ON CONFLICT(stock_ticker, timestamp)
+    # DO UPDATE SET price = EXCLUDED.price
+    # """
 
-    # Execute the upsert operation
-    batch_df.sparkSession.sql(upsert_sql)
+    # # Execute the upsert operation
+    # batch_df.sparkSession.sql(upsert_sql)
 
     # # Write to PostgreSQL
     batch_df.write \
         .jdbc(url=jdbc_url, table="test_write", mode="append", properties=properties)
 
-
-if __name__ == "__main__":
+def run_pipeline():
+    # change current dir to spark_pipeline dev directory
+    os.chdir('/opt/airflow/spark_pipeline') # COMMENT OUT IF NOT RUNNING VIA AIRFLOW
     spark = create_spark_session()
     process_data(spark)
+
+if __name__ == "__main__":
+    run_pipeline()
